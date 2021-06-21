c774741203d8d108c349201f08a5e58812705bf1
Giao Vu Dinh <tvudinh20@gmail.com>
2021-06-21 21:57:03 +0800

    working modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 
 import './Modal.css';
 
-const Backdrop = props => {
-  return <div className='backdrop' onClick={props.onClose} />;
-};
+const Modal = props => {
+  const node = useRef();
+  const { onClose } = props;
 
-const ModalOverlay = props => {
+  // closing the modal
+  const handleClickOutside = useCallback(
+    event => {
+      if (node.current.contains(event.target)) {
+        return;
+      }
+      onClose();
+    },
+    [onClose]
+  );
+
+  // set up event listeners upon modal opening
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   return (
     <div className='modal-container'>
-      <div className='modal'>{props.children}</div>
+      <div className='modal' ref={node}>
+        {props.children}
+      </div>
     </div>
-  );
-};
-
-const Modal = props => {
-  return (
-    <React.Fragment>
-      <Backdrop onClose={props.onClose} />
-      <ModalOverlay onClose={props.onClose}>{props.children}</ModalOverlay>
-    </React.Fragment>
   );
 };
 
