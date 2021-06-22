7dbb7e146ddbaf53cc2fb0dc36ef2a6b79b960bd
Giao Vu Dinh <tvudinh20@gmail.com>
2021-06-22 19:47:42 +0800

    footer

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -3,16 +3,17 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer>
+      <hr />
       <a
         className='footer__link'
-        href='https://github.com/palampinen/replayify'
-        target='_blank'
-        title='Replayify on Github'
-        rel='noreferrer'
+        href='https://github.com/tvu20/pokesearch'
+        // target='_blank'
+        title='PokeSearch on Github'
+        // rel='noreferrer'
         // rel='noreferrer'
       >
         <span
-          class='iconify'
+          className='iconify'
           data-icon='ion-social-github'
           data-inline='false'
         ></span>
